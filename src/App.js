535e146c2806374f2b2fbc3bb10e26705fd12066
Mohammad-Almomani <moh.moma.mmm@gmail.com>
2022-07-31 20:50:29 +0300

    added alert

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 class App extends Component {
   constructor (props){
     super(props);
@@ -20,22 +19,26 @@ class App extends Component {
     e.preventDefault();
     console.log(e.target.userSearch.value);
     const key = (process.env.REACT_APP_MAP_API_KEY);
+    if (e.target.userSearch.value !=''){
+      const cityInfo = await axios.get(`https://eu1.locationiq.com/v1/search?key=${key}&q=${e.target.userSearch.value}&format=json`);
+      console.log(cityInfo.data.length);
+      
+      
+      this.setState({user: cityInfo.data[0]})
+      
+      const res = await fetch(`https://maps.locationiq.com/v3/staticmap?key=${key}&center=${cityInfo.data[0].lat},${cityInfo.data[0].lon}&zoom=`);
+      const imageBlob = await res.blob();
+      const imageObjectURL = URL.createObjectURL(imageBlob);
+      this.setState({image: imageObjectURL});
 
-    const cityInfo = await axios.get(`https://eu1.locationiq.com/v1/search?key=${key}&q=${e.target.userSearch.value}&format=json`);
-    this.setState({user: cityInfo.data[0]})
-    console.log(process.env.REACT_APP_MAP_API_KEY);
-
-
-    
-    const res = await fetch(`https://maps.locationiq.com/v3/staticmap?key=${key}&center=${cityInfo.data[0].lat},${cityInfo.data[0].lon}&zoom=1-18`);
-    const imageBlob = await res.blob();
-    const imageObjectURL = URL.createObjectURL(imageBlob);
-    this.setState({image: imageObjectURL});
+    }
+    else return alert("City name can't be empty")
 
   }
   render () {
     return (
       <div className="App">
+        
       <div>
         <br></br>
       <h1>City Explore</h1>
@@ -49,11 +52,12 @@ class App extends Component {
       <br></br>
     </div>
         <div>
+          
         <Tabs
       defaultActiveKey="home"
       id="uncontrolled-tab-example"
       className="mb-3"
-    >
+    > 
       <Tab eventKey="home" title="City Name">
       <p> {this.state.user.display_name}</p>
       </Tab>
@@ -61,15 +65,13 @@ class App extends Component {
       <p>City latitude {this.state.user.lat} </p>
       <p>City longitude {this.state.user.lon}</p> 
       </Tab>
-      {/* <Tab eventKey="contact" title="Contact" disabled>
-      </Tab> */}
     </Tabs>
-        
-          
-         
+
           
         </div>
       <img src={this.state.image} />
+
+
     </div>
   );
 }
